b82adea5534a06039f0e1514bbcad98db6bb0ca4
KimrAndrew <andrewrk1997@gmail.com>
2021-11-13 14:03:25 -0800

    added logout button that is only visible when user is logged in

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -1,5 +1,6 @@
-import { Component } from "react";
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from 'react-bootstrap';
+{/*
 class LogoutButton extends Component {
 
   render() {
@@ -9,6 +10,22 @@ class LogoutButton extends Component {
       </button>
     );
   }
+};
+
+export default LogoutButton;
+*/}
+
+const LogoutButton = () => {
+  const { 
+    isAuthenticated,
+    logout,
+  } = useAuth0();
+
+  return isAuthenticated && (
+    <Button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </Button>
+  );
 };
 
 export default LogoutButton;
