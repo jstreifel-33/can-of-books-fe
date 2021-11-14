7c3ac65d6b0b2ae16db9ad35193d4a16203b93ee
KimrAndrew <andrewrk1997@gmail.com>
2021-11-13 16:57:57 -0800

    more fixes

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,28 +1,6 @@
 import { Button } from 'react-bootstrap'
 import { useAuth0 } from '@auth0/auth0-react';
 
-{/*
-export default class LoginButton extends Component {
-
-  
-
-  handleClick = () =>{
-    this.props.showForm();
-  }
-
-  
-
-  render() {
-
-    return (
-    <Button onClick={this.handleClick}>Log In</Button>
-    )
-  }
-}
-*/}
-
-
-
 function LoginButton() {
   const {
     isAuthenticated,
