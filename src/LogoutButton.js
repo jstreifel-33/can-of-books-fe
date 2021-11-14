efaeaafd14e97c8e2e2e3b7c337edaf4a22e0001
KimrAndrew <andrewrk1997@gmail.com>
2021-11-13 16:57:21 -0800

    removed code blocks

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -1,19 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from 'react-bootstrap';
-{/*
-class LogoutButton extends Component {
-
-  render() {
-    return (
-      <button onClick={this.props.onLogout}>
-        Log Out
-      </button>
-    );
-  }
-};
-
-export default LogoutButton;
-*/}
 
 const LogoutButton = () => {
   const { 
